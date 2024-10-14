72238e36d11a927087049c8ade3ed4ebd4a91f83
Ali El-Zein <shaclata@gmail.com>
2024-10-13 20:31:31 -0500

    Revert "Merge pull request #3 from tkhall03/backend"
    This reverts commit 49ad699a4c98225ba15357aca6293133bf9f2178, reversing
    changes made to 8475b40dd0083e79a5cd2f30c22a8cebc834e13f.

--- backend/Capstone/Program.cs
+++ backend/Capstone/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Capstone.Data;
-using Capstone.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,13 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddDbContext<SeaAggieCorpContext>(options =>
-    options.UseSqlServer("CapstoneDB"));
-
-
-builder.Services.AddScoped<IPersonService, PersonService>();
 
 var app = builder.Build();
 
