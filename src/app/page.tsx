d062f69e53c2d4cd65ecd19d32f3ddcf91ca5ba4
AsunaIsMyDaddy <93293753+tkhall03@users.noreply.github.com>
2024-11-20 08:41:55 -0600

    added login route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+'use client'
+
 import NavBar from "./components/NavBar";
 import Image from "next/image";
 
 import altDescriptions from "./data/altDescriptions.json";
 
+function logIn(){
+  window.location.href = "/classList";
+}
+
 export default function Home() {
+
+  function logIn(){
+    window.location.href = "/classList";
+  }
+
   return (
     <div className="flex flex-col h-screen">
       	<NavBar />
         <div className="relative flex-grow">
           <Image src="/mara.jpg" alt={altDescriptions.kennedy} layout="fill" objectFit="cover"/>
           <div className="absolute inset-0 flex items-center justify-center">
-            <button className="text-3xl font-bold px-20 py-6 bg-aggie-maroon text-white rounded-lg hover:bg-darker-aggie-maroon focus:outline-none">
+            <button className="text-3xl font-bold px-20 py-6 bg-aggie-maroon text-white rounded-lg hover:bg-darker-aggie-maroon focus:outline-none" onClick={() => logIn()}>
               Log In
             </button>
           </div>
